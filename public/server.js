fbd8e4399a47a05a4e718375ad38eb4408c98482
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-10-31 21:45:20 -0400

    changed get '/' to get '*' and placed at the end of the file so that if a path does not exist, we will default to the home page

--- public/server.js
+++ public/server.js
@@ -5,29 +5,29 @@ const apiData = require('../db/db.json');
 
 const app = express();
 
-// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
-// Home Page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 // Notes Page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
-// Api Data
+// Api Data - GET
 app.get('/api/notes', (req, res) => {
     res.json(apiData);
 });
 
+// Api Data - POST
 app.post('/api/notes', (req, res) => {
 
 })
+
+// If the path does not exist, send index.html (home page) - It is important to call this last so that it does not override the other routes/paths that we have designated
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // tell computer to listen to port for app to run
 app.listen(PORT, () =>
